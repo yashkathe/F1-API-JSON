7f783ffd729ecf873d2c682384a918463abf75e8
Matt443 <marcin26050606@gmail.com>
2025-06-20 19:37:46 +0200

    fix: uncomment previously broken functions now fixed

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-// export { getFastestLaps } from "./scraper/fastest-laps";
-// export { getDriverLineup } from "./scraper/driver-lineup";
-// export { getTeamLineup } from "./scraper/team-lineup";
-// export { getDriverStandings } from "./scraper/driver-standings";
-// export { getConstructorStandings } from "./scraper/constructors-standings";
-// export { getWorldChampions } from "./scraper/world-champions";
-// export { getRaceResults } from "./scraper/race-results";
-// export { getRaceSchedule } from "./scraper/race-schedule";
+export { getFastestLaps } from "./scraper/fastest-laps";
+export { getDriverLineup } from "./scraper/driver-lineup";
+export { getTeamLineup } from "./scraper/team-lineup";
+export { getDriverStandings } from "./scraper/driver-standings";
+export { getConstructorStandings } from "./scraper/constructors-standings";
+export { getWorldChampions } from "./scraper/world-champions";
+export { getRaceResults } from "./scraper/race-results";
+export { getRaceSchedule } from "./scraper/race-schedule";
 export { getFullRaceResults } from "./scraper/full-race-results";
