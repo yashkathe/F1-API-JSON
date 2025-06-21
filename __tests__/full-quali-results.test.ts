ed4ba1c8f6c98ee005b8c06af226a9fada995750
Matt443 <marcin26050606@gmail.com>
2025-06-21 21:10:27 +0200

    Updated snapshot

--- __tests__/full-quali-results.test.ts
+++ __tests__/full-quali-results.test.ts
@@ -1,5 +1,5 @@
 import { getFullQualiResults } from "../src/server";
 
 test("get full quali results", async () => {
-    expect(await getFullQualiResults(2000, "Great-Britain")).toMatchSnapshot();
+    expect(await getFullQualiResults(2008, "Great-Britain")).toMatchSnapshot();
 });
